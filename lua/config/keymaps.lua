539369e1a3c83b46681a8c24bffb53ababf03acc
Kamil Czarnecki <85490092+cyber-kamil@users.noreply.github.com>
2024-04-20 09:56:23 +0100

    Fix - Broken keymap fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.kevim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
